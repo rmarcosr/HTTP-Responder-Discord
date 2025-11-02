a20c86bf37e98f23ac104e12e9e55598c81ee87c
Marcos Rodríguez <marcosrodriguezroman700.02@gmail.com>
2025-11-02 20:56:52 +0100

    refactor: streamline API response handling and improve reply formatting

--- server.js
+++ server.js
@@ -26,31 +26,27 @@ client.on(Events.InteractionCreate, async (interaction) => {
             if (!response.ok) {
                 return await interaction.reply('Error calling the API');
             }
-            const data = await response.json()
 
+            const data = await response.json();
             const jsonString = JSON.stringify(data, null, 2);
 
             if (jsonString.length < 1900) {
                 return await interaction.reply({
-                    content: `**STATUS: ${response.status}\n\`\`\`json\n${jsonString}\n\`\`\``
+                    content: reply(response, jsonString)
                 });
             } else {
                 const filePath = './response.json';
-
                 fs.writeFileSync(filePath, jsonString);
-
                 const file = new AttachmentBuilder(filePath);
 
                 await interaction.reply({
-                    content: `STATUS: ${response.status}`,
+                    content: `STATUS: ${response.status} ** ${response.statusText} **`,
                     files: [file],
                 });
 
                 fs.unlinkSync(filePath);
-
                 return;
             }
-
         } catch (error) {
             return await interaction.reply(`Error: ${error.message}`);
         }
@@ -73,22 +69,19 @@ client.on(Events.InteractionCreate, async (interaction) => {
             const response = await fetch(url, {
                 method: 'POST',
                 body: JSON.stringify(body),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                }
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
             });
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API')
-            }
-            const data = await response.json()
 
+            if (!response.ok) {
+                return await interaction.reply('Error calling the API');
+            }
+
+            const data = await response.json();
             const jsonString = JSON.stringify(data, null, 2);
 
             return await interaction.reply({
-                content: `**STATUS: ${response.status}\n\`\`\`json\n${jsonString}\n\`\`\``
+                content: reply(response, jsonString)
             });
-
-
         } catch (error) {
             return await interaction.reply(`Error: ${error.message}`);
         }
@@ -111,22 +104,19 @@ client.on(Events.InteractionCreate, async (interaction) => {
             const response = await fetch(url, {
                 method: 'PUT',
                 body: JSON.stringify(body),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                }
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
             });
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API')
-            }
-            const data = await response.json();
 
+            if (!response.ok) {
+                return await interaction.reply('Error calling the API');
+            }
+
+            const data = await response.json();
             const jsonString = JSON.stringify(data, null, 2);
 
             return await interaction.reply({
-                content: `STATUS: ${response.status}\n\`\`\`json\n${jsonString}\n\`\`\``
+                content: reply(response, jsonString)
             });
-
-
         } catch (error) {
             return await interaction.reply(`Error: ${error.message}`);
         }
@@ -138,29 +128,28 @@ client.on(Events.InteractionCreate, async (interaction) => {
         try {
             const response = await fetch(url, {
                 method: 'DELETE',
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                }
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
             });
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API')
-            }
-            const data = await response.json()
 
+            if (!response.ok) {
+                return await interaction.reply('Error calling the API');
+            }
+
+            const data = await response.json();
             const jsonString = JSON.stringify(data, null, 2);
 
-
             return await interaction.reply({
-                content: `**STATUS: ${response.status}\n\`\`\`json\n${jsonString}\n\`\`\``
+                content: reply(response, jsonString)
             });
-
-
         } catch (error) {
             return await interaction.reply(`Error: ${error.message}`);
         }
     }
-
 });
 
-
 client.login(process.env.DISCORD_TOKEN)
+
+
+function reply(response, data) {
+    return `STATUS: ${response.status} ** ${response.statusText} ** \n\`\`\`json\n${data}\n\`\`\``
+}
