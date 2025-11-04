aa76e794e89f72bd34f1369119a0c1a0cd439984
Marcos Rodríguez <marcosrodriguezroman700.02@gmail.com>
2025-11-04 20:39:42 +0100

    refactor: unificate API command handling into a single function

--- server.js
+++ server.js
@@ -18,160 +18,85 @@ client.once(Events.ClientReady, (c) => {
 client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'get') {
-        const url = interaction.options.getString('url');
-        const token = interaction.options.getString('token');
+    const petitionType = interaction.commandName.toUpperCase();
 
-        const headers = {};
-        if (token) {
-            headers['Authorization'] = `Bearer ${token}`;
-        }
+    const hasBody = ["POST", "PUT"].includes(petitionType);
 
-        try {
-            const response = await fetch(url, { headers });
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API');
-            }
+    await discordFetch(interaction, petitionType, hasBody);
 
-            const data = await response.json();
-            const jsonString = JSON.stringify(data, null, 2);
-
-            if (jsonString.length < 1900) {
-                return await interaction.reply({
-                    content: reply(response, jsonString)
-                });
-            } else {
-                const filePath = './response.json';
-                fs.writeFileSync(filePath, jsonString);
-                const file = new AttachmentBuilder(filePath);
-
-                await interaction.reply({
-                    content: `STATUS: ${response.status} ** ${response.statusText} **`,
-                    files: [file],
-                });
-
-                fs.unlinkSync(filePath);
-                return;
-            }
-        } catch (error) {
-            return await interaction.reply(`Error: ${error.message}`);
-        }
-    }
-
-    if (interaction.commandName === 'post') {
-        const url = interaction.options.getString('url');
-        const bodyInput = interaction.options.getString('body');
-        const token = interaction.options.getString('token');
-
-
-        let body = {};
-        if (bodyInput) {
-            try {
-                body = JSON.parse(bodyInput);
-            } catch (error) {
-                return await interaction.reply(`Error: ${error.message}`);
-            }
-        }
-        const headers = {
-            'Content-Type': 'application/json; charset=UTF-8',
-        };
-
-        if (token) {
-            headers['Authorization'] = `Bearer ${token}`;
-        }
-
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(body),
-                headers,
-            });
-
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API');
-            }
-
-            const data = await response.json();
-            const jsonString = JSON.stringify(data, null, 2);
-
-            return await interaction.reply({
-                content: reply(response, jsonString)
-            });
-        } catch (error) {
-            return await interaction.reply(`Error: ${error.message}`);
-        }
-    }
-
-    if (interaction.commandName === 'put') {
-        const url = interaction.options.getString('url');
-        const bodyInput = interaction.options.getString('body');
-        const token = interaction.options.getString('token');
-
-        let body = {};
-        if (bodyInput) {
-            try {
-                body = JSON.parse(bodyInput);
-            } catch (error) {
-                return await interaction.reply(`Error: ${error.message}`);
-            }
-        }
-
-        try {
-            const headers = { 'Content-type': 'application/json; charset=UTF-8' };
-            if (token) headers['Authorization'] = `Bearer ${token}`;
-
-            const response = await fetch(url, {
-                method: 'PUT',
-                body: JSON.stringify(body),
-                headers
-            });
-
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API');
-            }
-
-            const data = await response.json();
-            const jsonString = JSON.stringify(data, null, 2);
-
-            return await interaction.reply({
-                content: reply(response, jsonString)
-            });
-        } catch (error) {
-            return await interaction.reply(`Error: ${error.message}`);
-        }
-    }
-
-    if (interaction.commandName === 'delete') {
-        const url = interaction.options.getString('url');
-        const token = interaction.options.getString('token');
-
-        try {
-            const headers = { 'Content-type': 'application/json; charset=UTF-8' };
-            if (token) headers['Authorization'] = `Bearer ${token}`;
-
-            const response = await fetch(url, {
-                method: 'DELETE',
-                headers
-            });
-
-            if (!response.ok) {
-                return await interaction.reply('Error calling the API');
-            }
-
-            const data = await response.json();
-            const jsonString = JSON.stringify(data, null, 2);
-
-            return await interaction.reply({
-                content: reply(response, jsonString)
-            });
-        } catch (error) {
-            return await interaction.reply(`Error: ${error.message}`);
-        }
-    }
 });
 
 client.login(process.env.DISCORD_TOKEN)
 
+async function discordFetch(interaction, petitionType, hasBody) {
+    const url = interaction.options.getString('url');
+    const token = interaction.options.getString('token');
+
+
+    // Only POST/PUT Petition includes a body
+    let body = null;
+    if (hasBody) {
+        const bodyInput = interaction.options.getString('body');
+
+        body = {};
+        if (bodyInput) {
+            try {
+                body = JSON.parse(bodyInput);
+            } catch (error) {
+                return await interaction.reply(`Error: ${error.message}`);
+            }
+        }
+    }
+
+    // Validate the header of petition
+    const headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+    };
+
+    if (token) {
+        headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: petitionType,
+            headers,
+            ...(hasBody && body ? { body: JSON.stringify(body) } : {}),
+        });
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`STATUS: ${response.status} **${response.statusText} ** \n\`\`\`json\n${errorText || response.statusText}\n\`\`\``);
+        }
+
+        // Preparing the response
+        const data = await response.json();
+
+        const jsonString = JSON.stringify(data, null, 2);
+
+        // The max lengt of Discord is 2000, if response is more, create a temporal file to insert
+        if (jsonString.length < 1900) {
+            return await interaction.reply({
+                content: reply(response, jsonString)
+            });
+        } else {
+            const filePath = './response.json';
+            fs.writeFileSync(filePath, jsonString);
+            const file = new AttachmentBuilder(filePath);
+
+            await interaction.reply({
+                content: `STATUS: ${response.status} ** ${response.statusText} **`,
+                files: [file],
+            });
+
+            //Delete the file
+            fs.unlinkSync(filePath);
+            return;
+        }
+    } catch (error) {
+        return await interaction.reply(`${error.message}`);
+    }
+}
 
 function reply(response, data) {
     return `STATUS: ${response.status} ** ${response.statusText} ** \n\`\`\`json\n${data}\n\`\`\``
